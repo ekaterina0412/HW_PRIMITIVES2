8fc2b4bbb4002bc8f73f8729087041c6c06229d8
Kate <avdoshka1@yandex.ru>
2023-11-06 20:09:56 +0400

    Comment

--- src/Main.java
+++ src/Main.java
@@ -5,20 +5,19 @@ public class Main {
         // параметров программы: начального счёта,
         // суммы пополнения и тп
         int account = 100;
-        int sum = 1100;
+        int sum = 300;
         int bonus;
 
         // Условным оператором проверяете, превысила ли
         // сумма пополнения порог, и для этих двух разных
         // сценариев рассчитываете сумму бонуса и выводите
         // на экран.
-        if (sum > 1000){
-            bonus = sum/100;
-        }
-        else{
+        if (sum > 1000) {
+            bonus = sum / 100;
+        } else {
             bonus = 0;
         }
         account = account + sum + bonus;
-        System.out.println("Итоговый счет: " + account + ". Бонусных рублей: "+ bonus);
+        System.out.println("Итоговый счет: " + account + ". Бонусных рублей: " + bonus);
     }
 }
